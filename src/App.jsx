6e177afb60534582cfd8d351a34aab38ebece9bb
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2025-01-26 14:20:24 +0530

    add: router and page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/Home'
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import SendMoney from "./pages/SendMoney"
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={ <Login/> } />
+          <Route path='/signup' element={ <Signup/> } />
+          <Route path='/dashboard' element={ <Dashboard/> } />
+          <Route path='/send' element={ <SendMoney/> } />
+
+        </Routes>
+      </BrowserRouter>
+    
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,30 +6,32 @@ import Footer from '../components/Footer'
 const Home = () => {
   return (
     <div>
-        <div className='container mx-auto px-5 md:px-20 lg:px-48'>
+        
+        <div className='container mx-auto px-5 md:px-16 lg:px-48'>
             <Navbar/>
         </div>
+        
         
         <div className='container mx-auto px-5 md:px-20 lg:px-48'>
             <div className='flex flex-col-reverse lg:flex-row lg:items-center lg:justify-between mb-20'>
                 <div className='flex flex-col items-center justify-center lg:justify-start lg:items-start'> 
 
-                    <h1 className='text-4xl md:text-6xl font-bold mt-5 text-center leading-normal md:text-start'>
+                    <h1 className='text-4xl md:text-6xl font-bold mt-10 lg:mt-5 text-center leading-normal md:text-start'>
                         Token Transfer
                     </h1>
-                    <p className='text-lg text-center lg:w-full lg:text-start w-64 md:mt-5 text-gray-700'>Payments made Simple and Easier</p>
+                    <p className='text-lg text-center lg:w-full lg:text-start w-64 mt-10 md:mt-5 text-gray-700'>A fun project based on real working of payment app</p>
 
                     <Button 
                     title='Check Out'
-                    classProp="mt-4"
+                    classProp="mt-10 lg:mt-4"
                     />
                 </div>
 
-                <div className='flex items-center justify-center'> 
+                <div className='flex items-center justify-center mt-10 lg:mt-0'> 
                     <img
-                        src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-mobile-money-transferring-banking-concept-vector-hand-holding-smartphone-dollar-and-png-image_5204362.jpg" 
+                        src= 'https://png.pngtree.com/png-clipart/20200224/original/pngtree-mobile-money-transferring-banking-concept-vector-hand-holding-smartphone-dollar-and-png-image_5204362.jpg'
                         alt="hero img"
-                        className='h-[350px] md:h-[600px] rounded-3xl'
+                        className='h-[400px] md:h-[600px] rounded-3xl object-contain'
                     />
                 </div>
             </div>
